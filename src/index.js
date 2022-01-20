087028ff6e3bba5e7a7fc6c74f063693ba5befc4
Akash Pateliya <akash.pateliya@gmail.com>
2022-01-21 00:31:41 +0530

    updated index.js

--- src/index.js
+++ src/index.js
@@ -10,7 +10,8 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(requestLogger);
 
 routes(app);
+const PORT = process.env.PORT || 8080;
 
-var server = app.listen(process.env.PORT || 3000, function () {
+var server = app.listen(PORT, function () {
     console.log("app running on port.", server.address().port);
 });
