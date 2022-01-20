18be12841f9ef648f19cf3bda2168df1af7d02a7
Akash Pateliya <akash.pateliya@gmail.com>
2022-01-20 23:07:38 +0530

    add-data and delete-data implemented

--- routes/routes.js
+++ routes/routes.js
@@ -1,8 +1,8 @@
 const environment = require("../env/environment");
 // Google sheet npm package
-const { GoogleSpreadsheet } = require('google-spreadsheet');
+const { GoogleSpreadsheet } = require("google-spreadsheet");
 // File handling package
-const fs = require('fs');
+const fs = require("fs");
 // spreadsheet key is the long id in the sheets URL
 const RESPONSES_SHEET_ID = environment.spreadsheetId;
 // Create a new document
@@ -16,38 +16,79 @@ var appRouter = function (app) {
     res.status(200).send(result);
   });
 
-app.get("/getdata",async function (req, res) {
-        await doc.useServiceAccountAuth({
-            client_email: environment.client_email,
-            private_key: environment.private_key
-        });
+  app.get("/get-data", async function (req, res) {
+    await doc.useServiceAccountAuth({
+      client_email: environment.client_email,
+      private_key: environment.private_key,
+    });
+
+    // load the documents info
+    await doc.loadInfo();
+
+    // Index of the sheet
+    let sheet = doc.sheetsByIndex[0];
+
+    // Get all the rows
+    let rows = await sheet.getRows();
+
+    result = [];
+    for (let index = 0; index < rows.length; index++) {
+      const row = rows[index];
+      const obj = {
+        MatchNo: Number(row.MatchNo),
+        MatchDateTime: row.MatchDateTime,
+        Tour: row.Tour,
+        Round: row.Round,
+        Investment: Number(row.Investment),
+        Winnings: Number(row.Winnings),
+        ProfitOrLoss: Number(row.ProfitOrLoss),
+      };
+      result.push(obj);
+    }
+    res.status(200).send(result);
+  });
+
+  app.post("/add-data", async function (req, res) {
+    await doc.useServiceAccountAuth({
+      client_email: environment.client_email,
+      private_key: environment.private_key,
+    });
+
+    // load the documents info
+    await doc.loadInfo();
+
+    // Index of the sheet
+    let sheet = doc.sheetsByIndex[0];
+    const result = await sheet.addRow(req.body);
+    res.status(200).send(result);
+  });
+
+  app.post("/delete-data", async function (req, res) {
+    await doc.useServiceAccountAuth({
+      client_email: environment.client_email,
+      private_key: environment.private_key,
+    });
+
+    // load the documents info
+    await doc.loadInfo();
+
+    // Index of the sheet
+    let sheet = doc.sheetsByIndex[0];
+
+    // Get all the rows
+    let rows = await sheet.getRows();
+
+    let result = null;
+    for (let index = 0; index < rows.length; index++) {
+      const row = rows[index];
+      if (row[req.body.keyValue] == req.body.thisValue) {
+        result = await rows[index].delete();
+        break;
+      }
+    }
     
-        // load the documents info
-        await doc.loadInfo();
-    
-        // Index of the sheet
-        let sheet = doc.sheetsByIndex[0];
-    
-        // Get all the rows
-        let rows = await sheet.getRows();
-        
-        result = [];
-        for (let index = 0; index < rows.length; index++) {
-            const row = rows[index];
-            const obj = {
-                MatchNo : Number(row.MatchNo),
-                MatchDateTime: row.MatchDateTime,
-                Tour: row.Tour,
-                Round: row.Round,
-                Investment: Number(row.Investment),
-                Winnings: Number(row.Winnings),
-                ProfitOrLoss: Number(row.ProfitOrLoss),
-            }
-            result.push(obj);
-        };
     res.status(200).send(result);
   });
 };
-
 
 module.exports = appRouter;
