a164355d405cdb120b04a64eb343fcf31f547f21
Akash Pateliya <akash.pateliya@gmail.com>
2022-01-21 00:21:28 +0530

    updated index.js

--- src/index.js
+++ src/index.js
@@ -11,6 +11,6 @@ app.use(requestLogger);
 
 routes(app);
 
-var server = app.listen(3000, function () {
+var server = app.listen(process.env.PORT | 3000, function () {
     console.log("app running on port.", server.address().port);
 });
