aeaff4926a366d09e75577ede18e85793f74834c
Akash Pateliya <akash.pateliya@gmail.com>
2022-01-21 23:19:23 +0530

    cors added

--- src/index.js
+++ src/index.js
@@ -2,8 +2,10 @@ var express = require("express");
 var bodyParser = require("body-parser");
 var routes = require("../routes/routes");
 const requestLogger = require("./logger/request.logger");
+var cors = require('cors');
 var app = express();
 
+app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
