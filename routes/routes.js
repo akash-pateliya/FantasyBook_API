570309a07017656453c62a5783563866106dd3e8
Akash Pateliya <akash.pateliya@gmail.com>
2022-01-21 23:37:07 +0530

    get api issue fix

--- routes/routes.js
+++ routes/routes.js
@@ -47,7 +47,7 @@ var appRouter = function (app) {
         };
         result.push(obj);
       }
-      res.status(StatusCodes.INTERNAL_SERVER_ERROR).send(result);
+      res.status(StatusCodes.OK).send(result);
     } catch (error) {
       res.status(StatusCodes.INTERNAL_SERVER_ERROR).send(error);
     }
