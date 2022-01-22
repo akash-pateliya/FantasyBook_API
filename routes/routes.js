3db4e5e31ce1cf19e3a05948640889b50dabef3c
Akash Pateliya <akash.pateliya@gmail.com>
2022-01-23 00:30:19 +0530

    mongo db implemented

--- routes/routes.js
+++ routes/routes.js
@@ -1,13 +1,5 @@
-const environment = require("../src/env/environment");
-// Google sheet npm package
-const { GoogleSpreadsheet } = require("google-spreadsheet");
-// File handling package
-const fs = require("fs");
+const { MongoClient } = require("mongodb");
 const { StatusCodes } = require("http-status-codes");
-// spreadsheet key is the long id in the sheets URL
-const RESPONSES_SHEET_ID = environment.spreadsheetId;
-// Create a new document
-const doc = new GoogleSpreadsheet(RESPONSES_SHEET_ID);
 
 var appRouter = function (app) {
   app.get("/", function (req, res) {
@@ -19,35 +11,28 @@ var appRouter = function (app) {
 
   app.get("/get-data", async function (req, res) {
     try {
-      await doc.useServiceAccountAuth({
-        client_email: environment.client_email,
-        private_key: environment.private_key,
+      const uri =
+        "mongodb+srv://root:root@fantasybook.qxgk4.mongodb.net/FantasyBook?retryWrites=true&w=majority";
+      const client = new MongoClient(uri, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
       });
-
-      // load the documents info
-      await doc.loadInfo();
-
-      // Index of the sheet
-      let sheet = doc.sheetsByIndex[0];
-
-      // Get all the rows
-      let rows = await sheet.getRows();
-
-      result = [];
-      for (let index = 0; index < rows.length; index++) {
-        const row = rows[index];
-        const obj = {
-          MatchNo: Number(row.MatchNo),
-          MatchDateTime: row.MatchDateTime,
-          Tour: row.Tour,
-          Round: row.Round,
-          Investment: Number(row.Investment),
-          Winnings: Number(row.Winnings),
-          ProfitOrLoss: Number(row.ProfitOrLoss),
-        };
-        result.push(obj);
-      }
-      res.status(StatusCodes.OK).send(result);
+      client.connect(async (error) => {
+        if (error) {
+          res.status(StatusCodes.INTERNAL_SERVER_ERROR).send(error);
+        }
+        await client
+          .db("FantasyBook")
+          .collection("records")
+          .find()
+          .toArray(function (error, result) {
+            if (error) {
+              res.status(StatusCodes.INTERNAL_SERVER_ERROR).send(error);
+            }
+            res.status(StatusCodes.OK).send(result);
+          });
+        r;
+      });
     } catch (error) {
       res.status(StatusCodes.INTERNAL_SERVER_ERROR).send(error);
     }
@@ -55,18 +40,22 @@ var appRouter = function (app) {
 
   app.post("/add-data", async function (req, res) {
     try {
-      await doc.useServiceAccountAuth({
-        client_email: environment.client_email,
-        private_key: environment.private_key,
+      const uri =
+        "mongodb+srv://root:root@fantasybook.qxgk4.mongodb.net/FantasyBook?retryWrites=true&w=majority";
+      const client = new MongoClient(uri, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
       });
-
-      // load the documents info
-      await doc.loadInfo();
-
-      // Index of the sheet
-      let sheet = doc.sheetsByIndex[0];
-      await sheet.addRow(req.body);
-      res.status(StatusCodes.OK).send({ status: true });
+      client.connect(async (error) => {
+        if (error) {
+          res.status(StatusCodes.INTERNAL_SERVER_ERROR).send(error);
+        }
+        await client
+          .db("FantasyBook")
+          .collection("records")
+          .insertOne(req.body);
+        res.status(StatusCodes.OK).send({ status: true });
+      });
     } catch (error) {
       res.status(StatusCodes.INTERNAL_SERVER_ERROR).send(error);
     }
@@ -74,29 +63,22 @@ var appRouter = function (app) {
 
   app.post("/delete-data", async function (req, res) {
     try {
-      await doc.useServiceAccountAuth({
-        client_email: environment.client_email,
-        private_key: environment.private_key,
+      const uri =
+        "mongodb+srv://root:root@fantasybook.qxgk4.mongodb.net/FantasyBook?retryWrites=true&w=majority";
+      const client = new MongoClient(uri, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
       });
-
-      // load the documents info
-      await doc.loadInfo();
-
-      // Index of the sheet
-      let sheet = doc.sheetsByIndex[0];
-
-      // Get all the rows
-      let rows = await sheet.getRows();
-
-      let result = null;
-      for (let index = 0; index < rows.length; index++) {
-        const row = rows[index];
-        if (row[req.body.keyValue] == req.body.thisValue) {
-          result = await rows[index].delete();
-          break;
+      client.connect(async (error) => {
+        if (error) {
+          res.status(StatusCodes.INTERNAL_SERVER_ERROR).send(error);
         }
-      }
-      res.status(StatusCodes.OK).send({ status: true });
+        await client
+          .db("FantasyBook")
+          .collection("records")
+          .deleteOne({ MatchNo: req.body.thisValue });
+        res.status(StatusCodes.OK).send({ status: true });
+      });
     } catch (error) {
       res.status(StatusCodes.INTERNAL_SERVER_ERROR).send(error);
     }
