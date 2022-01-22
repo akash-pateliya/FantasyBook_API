7fc7fc556ca9401b810b2e363a371955ee042d88
Akash Pateliya <akash.pateliya@gmail.com>
2022-01-23 02:01:14 +0530

    code optimize and tour-lib added

--- routes/routes.js
+++ routes/routes.js
@@ -2,6 +2,13 @@ const { MongoClient } = require("mongodb");
 const { StatusCodes } = require("http-status-codes");
 
 var appRouter = function (app) {
+  const uri =
+    "mongodb+srv://root:root@fantasybook.qxgk4.mongodb.net/FantasyBook?retryWrites=true&w=majority";
+  const client = new MongoClient(uri, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  });
+
   app.get("/", function (req, res) {
     const result = {
       "/": "/",
@@ -11,21 +18,15 @@ var appRouter = function (app) {
 
   app.get("/get-data", async function (req, res) {
     try {
-      const uri =
-        "mongodb+srv://root:root@fantasybook.qxgk4.mongodb.net/FantasyBook?retryWrites=true&w=majority";
-      const client = new MongoClient(uri, {
-        useNewUrlParser: true,
-        useUnifiedTopology: true,
-      });
       client.connect(async (error) => {
         if (error) {
           res.status(StatusCodes.INTERNAL_SERVER_ERROR).send(error);
         }
-        function compare( a, b ) {
-          if ( a.MatchNo < b.MatchNo ){
+        function compare(a, b) {
+          if (a.MatchNo < b.MatchNo) {
             return -1;
           }
-          if ( a.MatchNo > b.MatchNo ){
+          if (a.MatchNo > b.MatchNo) {
             return 1;
           }
           return 0;
@@ -50,12 +51,6 @@ var appRouter = function (app) {
 
   app.post("/add-data", async function (req, res) {
     try {
-      const uri =
-        "mongodb+srv://root:root@fantasybook.qxgk4.mongodb.net/FantasyBook?retryWrites=true&w=majority";
-      const client = new MongoClient(uri, {
-        useNewUrlParser: true,
-        useUnifiedTopology: true,
-      });
       client.connect(async (error) => {
         if (error) {
           res.status(StatusCodes.INTERNAL_SERVER_ERROR).send(error);
@@ -77,12 +72,6 @@ var appRouter = function (app) {
 
   app.post("/delete-data", async function (req, res) {
     try {
-      const uri =
-        "mongodb+srv://root:root@fantasybook.qxgk4.mongodb.net/FantasyBook?retryWrites=true&w=majority";
-      const client = new MongoClient(uri, {
-        useNewUrlParser: true,
-        useUnifiedTopology: true,
-      });
       client.connect(async (error) => {
         if (error) {
           res.status(StatusCodes.INTERNAL_SERVER_ERROR).send(error);
@@ -101,6 +90,56 @@ var appRouter = function (app) {
       res.status(StatusCodes.INTERNAL_SERVER_ERROR).send(error);
     }
   });
+
+  app.get("/get-lib-tour", function (req, res) {
+    client.connect(async (error) => {
+      if (error) {
+        res.status(StatusCodes.INTERNAL_SERVER_ERROR).send(error);
+      }
+      await client
+        .db("FantasyBook")
+        .collection("lib-tour")
+        .find()
+        .toArray(function (error, result) {
+          if (error) {
+            res.status(StatusCodes.INTERNAL_SERVER_ERROR).send(error);
+          }
+          console.log(result);
+          resultArr = [];
+          result.forEach((data) => {
+            console.log(data);
+            resultArr.push(data.Tour);
+          });
+          res.status(StatusCodes.OK).send(resultArr);
+        });
+      r;
+    });
+  });
+
+  app.post("/add-lib-tour", function (req, res) {
+    try {
+      client.connect(async (error) => {
+        if (error) {
+          res.status(StatusCodes.INTERNAL_SERVER_ERROR).send(error);
+        }
+        await client
+          .db("FantasyBook")
+          .collection("lib-tour")
+          .insertOne(req.body, function (error, result) {
+            if (error) {
+              res.status(StatusCodes.INTERNAL_SERVER_ERROR).send(error);
+            }
+            res.status(StatusCodes.OK).send(result);
+          });
+      });
+    } catch (error) {
+      res.status(StatusCodes.INTERNAL_SERVER_ERROR).send(error);
+    }
+  });
+
+  app.post("/update-lib-tour", function (req, res) {});
+
+  app.post("/delete-lib-tour", function (req, res) {});
 };
 
 module.exports = appRouter;
